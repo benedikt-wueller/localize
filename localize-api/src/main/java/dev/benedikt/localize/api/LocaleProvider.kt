ef3c6d86ab40e604f7a516528062f528144dc763
Benedikt Wüller <wuelleronline@gmail.com>
2021-03-22 10:18:18 +0100

    Clarify

--- localize-api/src/main/java/dev/benedikt/localize/api/LocaleProvider.kt
+++ localize-api/src/main/java/dev/benedikt/localize/api/LocaleProvider.kt
@@ -12,18 +12,24 @@ interface LocaleProvider {
      * If [force] is `false`, the provider will unload the strings, when they are not required anymore.
      *
      * In general, strings are required if they have been accessed lately or are somehow assigned to an object (context).
+     *
+     * This method should not be called manually as the [dev.benedikt.localize.LocalizeService] may override it's state.
      */
     fun setForceLoad(force: Boolean)
 
     /**
      * Tells the provider that one more source is requesting strings in the future.
      * If no sources requested the strings previously and the provider is not forced to load (see [setForceLoad]), they will be loaded.
+     *
+     * If you call this method manually, make sure to remove the requirement with [removeRequirement] at some point.
      */
     fun addRequirement()
 
     /**
      * Tells the provider that one less source is requesting strings in the future.
      * If no source requests the strings anymore, they will be unloaded, if the provider is not forced to load (see [setForceLoad]).
+     *
+     * Only call this method manually, if you previously called [addRequirement] manually as well.
      */
     fun removeRequirement()
 
