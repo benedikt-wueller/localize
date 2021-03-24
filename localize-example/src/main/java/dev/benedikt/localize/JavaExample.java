5eb32c41f127795ee1164789010dfd270f5c116c
Benedikt Wüller <wuelleronline@gmail.com>
2021-03-24 12:43:14 +0100

    Fix parent references and update example

--- localize-example/src/main/java/dev/benedikt/localize/JavaExample.java
+++ localize-example/src/main/java/dev/benedikt/localize/JavaExample.java
@@ -1,8 +1,7 @@
 package dev.benedikt.localize;
 
 import static dev.benedikt.localize.TranslationHelpersKt.*;
-import dev.benedikt.localize.yaml.YamlLocaleProvider;
-import java.nio.file.Paths;
+import dev.benedikt.localize.json.JsonHttpLocaleProvider;
 
 /**
  * @author Benedikt Wüller
@@ -10,16 +9,12 @@ import java.nio.file.Paths;
 public class JavaExample {
 
     public static void main(String[] args) throws InterruptedException {
-        LocalizeService.INSTANCE.provideLocale("en_EN", new YamlLocaleProvider(Paths.get("./localize-example/locales/en_EN.yaml")));
-        LocalizeService.INSTANCE.provideLocale("de_DE", new YamlLocaleProvider(Paths.get("./localize-example/locales/de_DE.json")));
-
+        LocalizeService.INSTANCE.provideLocale("en_EN", new JsonHttpLocaleProvider("https://raw.githubusercontent.com/i18next/ng-i18next/master/examples/locales/dev/translation.json"));
         LocalizeService.INSTANCE.setFallbackLocale("en_EN");
 
-        LocalizeService.INSTANCE.translate("de_DE", "common.hello2", "Bob", "Alice").thenAccept(System.out::println);
+        trans("en_EN", "directive.cloak.description").thenAccept(System.out::println);
 
-        trans("de_DE", "common.hello2", "Bob", "Alice").thenAccept(System.out::println);
-
-        Thread.sleep(1000);
+        Thread.sleep(5000);
     }
 
 }
